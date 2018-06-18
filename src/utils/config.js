da06f0be358b11255fe9bdcb6198faee0bf6b4a1
Ahmed Abdulrahman <a.kasapbashi@live.com>
2018-06-18 21:35:13 +0200

    :wrench: Configure env variables
    Move firebase secrets to .env

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,9 +1,9 @@
 // Use your firebase app config
 export const config = {
-  apiKey: 'FIREBASE_API_KEY',
-  authDomain: 'FIREBASE_AUTH_DOMAIN',
-  databaseURL: 'FIREBASE_DATABASE_URL',
-  projectId: 'FIREBASE_PROJECT_ID',
-  storageBucket: '',
-  messagingSenderId: 'FIREBASE_MESSAGING_ID',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
 }
