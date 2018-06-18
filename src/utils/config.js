87c248fc9f63425a8d47a1c843b6d61d7d5cf7e8
Ahmed Abdulrahman <a.kasapbashi@live.com>
2018-06-18 17:17:39 +0200

    :sparkles: Add firebase configration
    Authentication setup

---
+++ src/utils/config.js
@@ -0,0 +1,9 @@
+// Use your firebase app config
+export const config = {
+  apiKey: 'FIREBASE_API_KEY',
+  authDomain: 'FIREBASE_AUTH_DOMAIN',
+  databaseURL: 'FIREBASE_DATABASE_URL',
+  projectId: 'FIREBASE_PROJECT_ID',
+  storageBucket: '',
+  messagingSenderId: 'FIREBASE_MESSAGING_ID',
+}
